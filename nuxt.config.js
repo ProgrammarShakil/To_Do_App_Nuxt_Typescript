6b3b08241606d385f8718616ddcd5fe52266e37a
programmarshakil <shakljoka103@gmail.com>
2023-05-07 14:17:53 +0600

    nuxt an tsconfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxt/typescript-build'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
